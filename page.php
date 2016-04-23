29311bef436da5c6e68011fd9fa4d65256a3e4f9
James Pederson <james@jpederson.com>
2016-04-23 15:13:07 -0500

    Simplify basic page template.

--- page.php
+++ page.php
@@ -4,13 +4,9 @@ get_header();
 
 ?>
 
-	<?php the_large_title(); ?>
-
 	<?php the_showcase(); ?>
 	
-	<?php if ( has_cmb_value( 'left_content' ) ) { ?>
 	<div id="content" class="wrap group content-wide" role="main">
-	<?php } ?>
 		<?php 
 		if ( have_posts() ) :
 			while ( have_posts() ) : the_post(); 
@@ -18,9 +14,7 @@ get_header();
 			endwhile;
 		endif;
 		?>
-		<?php if ( has_cmb_value( 'left_content' ) ) { ?>
 		</div>
-<?php } ?>
 	</div><!-- #content -->
 
 <?php
