4b214931bec25b832a64aa56ab7cfdbd84008016
James Pederson <james@jpederson.com>
2016-04-23 17:06:37 -0500

    Adjust page template.

--- page-front.php
+++ page-front.php
@@ -45,9 +45,31 @@ get_header();
 	<div class="events group">
 		<div class="wrap content-wide">
 			<img src="<?php bloginfo('template_url') ?>/img/header-events.png">
-			<?php 
+			<div class="home-event-list">
+				<?php 
+				$args = array(
+					'cat' => '-3',
+					'posts_per_page' => 3
+				);
 
-			?>
+				// The Query
+				$the_query = new WP_Query( $args );
+
+				// The Loop
+				if ( $the_query->have_posts() ) {
+					echo '<ul>';
+					while ( $the_query->have_posts() ) {
+						$the_query->the_post();
+						echo '<li><a href="' . get_the_permalink() . '">' . get_the_post_thumbnail() . get_the_title() . '</a></li>';
+					}
+					echo '</ul>';
+				} else {
+					// no posts found
+				}
+				/* Restore original Post Data */
+				wp_reset_postdata();
+				?>
+			</div>
 		</div>
 	</div>
 
@@ -69,7 +91,7 @@ get_header();
 					echo '<ul>';
 					while ( $the_query->have_posts() ) {
 						$the_query->the_post();
-						echo '<li>' . get_the_post_thumbnail() . get_the_title() . '</li>';
+						echo '<li><a href="' . get_the_permalink() . '">' . get_the_post_thumbnail() . get_the_title() . '</a></li>';
 					}
 					echo '</ul>';
 				} else {
--- page.php
+++ page.php
@@ -6,7 +6,7 @@ get_header();
 
 	<?php the_showcase(); ?>
 	
-	<div id="content" class="wrap group content-wide" role="main">
+	<div id="content" class="wrap group content-narrow" role="main">
 		<?php 
 		if ( have_posts() ) :
 			while ( have_posts() ) : the_post(); 
